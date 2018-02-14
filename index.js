ec5eafd61bfdd73964904ec39911e76a9740a6db
Joe Greiner <jrgreiner115@gmail.com>
2018-02-14 17:37:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ function currentLine(line) {
 if (katzDeli.length === 0) {
 return ("The line is currently empty.")
 }
-else return ("The line is currently: ")
+else return ("The line is currently:" + line)
 
 
   
