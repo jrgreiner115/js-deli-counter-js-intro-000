52f59abfb9ec9859030b9b1098762ff8a8ff19e2
Joe Greiner <jrgreiner115@gmail.com>
2018-02-14 16:54:12 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,12 @@
+var katzDeli = [];
+
+function takeANumber(katzDeli,name) {
+  katzDeli.push(`${name}`);
+  return(`Welcome, ${name}. You are number ${katzDeli.length} in line.`);
+}
+function nowServing(katzDeli, name) {
+  let n = 0;
+  while (n < katzDeli.length) {n++}
+  if (katzDeli.length === 0);
+  return "There is nobody waiting to be served!";
+else (katz.Deli.lenth > 1)
