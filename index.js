c73ce1fa7f9a6dce6f7d43c4b3cc8701daeb83a3
Joe Greiner <jrgreiner115@gmail.com>
2018-02-14 17:35:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,9 @@ function currentLine(line) {
   while (i > katzDeli.length) {line.push(` `+[i+1]+`. `  + katzDeli[i])
   i++;
 }
-if (katz)
+if (katzDeli.length === 0);
+return ("The line is currently empty.")
+
   
   
   
